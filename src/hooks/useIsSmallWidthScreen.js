84c35c3e3da6d47c1dfd0ac054b873b6aaed3cd7
Jixun Wu <jixun.moe@gmail.com>
2022-11-06 17:05:42 +0000

    fix: SSR for docs rendering in a small device

--- src/hooks/useIsSmallWidthScreen.js
+++ src/hooks/useIsSmallWidthScreen.js
@@ -1,19 +1,29 @@
 import { useEffect, useState } from 'react';
 import { debounce } from 'lodash';
-
-const getViewportWidth = () => window.outerWidth;
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export const BREAK_POINT_SMALL = 576;
 export const BREAK_POINT_MEDIUM = 768;
 export const BREAK_POINT_LARGE = 992;
 export const BREAK_POINT_XLARGE = 1200;
 
+const getViewportWidth = () => window.outerWidth;
+const getDefaultViewportWidth = () => BREAK_POINT_LARGE;
+
 export function useWindowWidth() {
-  const [windowWidth, setWindowWidth] = useState(getViewportWidth);
+  const isBrowser = useIsBrowser();
+  const cachedGetCurrentViewpoerWidth = isBrowser
+    ? getViewportWidth
+    : getDefaultViewportWidth;
+  const [windowWidth, setWindowWidth] = useState(cachedGetCurrentViewpoerWidth);
+
+  useEffect(() => {
+    setWindowWidth(cachedGetCurrentViewpoerWidth);
+  }, [isBrowser]);
 
   useEffect(() => {
     const handleResize = debounce(() => {
-      setWindowWidth(getViewportWidth);
+      setWindowWidth(cachedGetCurrentViewpoerWidth);
     }, 200);
 
     window.addEventListener('resize', handleResize);
